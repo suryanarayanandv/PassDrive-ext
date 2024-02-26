f634bff4db6ed97254f23b6bbf2cceff214c239a
suryanarayanan.dv <suryanarayanan.dv@zohocorp.com>
2024-02-26 23:00:37 +0530

    chrome native host interface

--- src/background.js
+++ src/background.js
@@ -1,15 +1,26 @@
 const { getDomainAndSubdomain } = require("./utils");
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  let response = {
+    username: "",
+    password: "",
+  };
+
   // parse message
   if (String(message).includes("parent_url")) {
     let URL = String(message).split(":")[1];
     let domain_sub = getDomainAndSubdomain(URL);
 
-    // TODO: native app comms
-    // post domain_sub
-    // .... ref: notes
+    // TODO: native impl test + register
+    let port = chrome.runtime.connectNative("com.passdrive-service");
+    port.postMessage({ data: JSON.stringify(domain_sub) });
 
-    sendResponse("");
+    port.onMessage.addListener(function (msg) {
+      let data = JSON.parse(msg);
+      response.username = data.username;
+      response.password = data.password;
+    });
+
+    sendResponse(JSON.stringify(response));
   }
 });
